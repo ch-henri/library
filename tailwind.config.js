32c5c9393a80c56470bcf379e9368e68d9e8d562
Charles-Henri Deschenaux <charles-henri.deschenaux@devfactory.ch>
2021-08-25 19:25:24 +0200

    Setup project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './dist/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+    fontFamily: {
+      sans: ['CooperHewitt', 'sans-serif'],
+      display: ['lmroman', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
