480e4e1d323b66231b883fae97cadca15fd61ca0
Charles-Henri Deschenaux <charles-henri.deschenaux@devfactory.ch>
2021-09-01 18:11:50 +0200

    Start design

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+let books = [
+  {
+    title: 'book1',
+    author: 'the author',
+    pages: '202',
+    haveRead: true,
+  }
+];
+
+let title = document.querySelector('#title');
+
+title.textContent = books[0].title;
+
+// 
