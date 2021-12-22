0a52dd2bca0c5fa0ac0c5c6c4da941bd8d7c37a9
charles-henri <charles-henri.deschenaux@devfactory.ch>
2021-12-22 09:43:48 +0100

    Improve book card design

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+/*
 let books = [
   {
     title: 'book1',
@@ -16,6 +17,31 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let newCard = template.content.cloneNode(true);
   console.log({newCard});
 
-  
+
   bookList.appendChild(newCard);
 })
+*/
+
+
+function Book(title, author, pages, read){
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+    return title + author + pages + read
+  }
+}
+
+let myBook = new Book('Mytitle', 'Me', 100, true);
+// console.log(myBook.info());
+
+
+let sherlock = {
+  address: { city: 'London' },
+}
+
+sherlock.test = sherlock.address;
+sherlock.address.city = 'Paris'
+
+console.log(sherlock.test);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   purge: [
     './dist/*.html',
   ],
@@ -11,7 +12,7 @@ module.exports = {
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      //'lg': '1024px',
+      'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
       //'xl': '1280px',
